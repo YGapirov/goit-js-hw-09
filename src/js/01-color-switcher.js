4b2f960da071eb8dcdc4d39425c62d4b286c5e1f
Yurii <gapirov21@gmail.com>
2023-09-15 15:18:53 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
-console.log
+
+const startColorBtn = document.querySelector('button[data-start]');
+const stopColorBtn = document.querySelector('button[data-stop]');
+let timerId = null; //
+
+startColorBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    startColorBtn.disabled = true; // блокуєм повторне натискання батона
+});
+
+
+stopColorBtn.addEventListener("click", () => {
+    clearInterval(timerId);    
+        startColorBtn.disabled = false; //розблоковуємо доступ до батона після стопа
+
+});
